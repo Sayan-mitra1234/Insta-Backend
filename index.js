a11c3613b50611fc9954e31c41856fb7cb70720e
Sayan-mitra1234 <119873741+Sayan-mitra1234@users.noreply.github.com>
2022-12-17 20:14:41 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect(dbUrl, () => console.log("Database Connected"))
 const postRoutes = require('./routes/post')
 const cors = require('cors');
  const corsOptions = {
-      origin: 'https://insfrontend.onrender.com',
+      origin: 'http://localhost:3000/',
      credentials: true,            //access-control-allow-credentials:true
      optionSuccessStatus: 200
   }
@@ -39,3 +39,4 @@ app.use('/*', (req, res) => {
 
 
 app.listen(PORT, () => console.log('Server is on 3000', PORT));
+//https://insfrontend.onrender.com
