a6236aa714d0b4f4acd7539c12b27f6d5182d735
Sayan-mitra1234 <119873741+Sayan-mitra1234@users.noreply.github.com>
2022-12-17 10:23:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ mongoose.connect(dbUrl, () => console.log("Database Connected"))
 const postRoutes = require('./routes/post')
 const cors = require('cors');
  const corsOptions = {
-     origin: 'https://insta-frontend-gasx.onrender.com',
+     origin: 'https://instafrontend1234.onrender.com',
      credentials: true,            //access-control-allow-credentials:true
      optionSuccessStatus: 200
  }
