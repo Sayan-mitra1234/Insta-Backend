017c6f8c8bddc716d0a5021745841a1c22f98426
Sayan-mitra1234 <119873741+Sayan-mitra1234@users.noreply.github.com>
2022-12-18 19:39:48 +0530

    Update index.js

--- index.js
+++ index.js
@@ -11,19 +11,19 @@ const mongoose = require('mongoose');
 mongoose.connect(dbUrl, () => console.log("Database Connected"))
 const postRoutes = require('./routes/post')
 const cors = require('cors');
- const corsOptions = {
-      origin: 'https://insfrontend.onrender.com',
-     credentials: true,            //access-control-allow-credentials:true
-     optionSuccessStatus: 200
-  }
-app.use(cors(corsOptions));
+//  const corsOptions = {
+//       origin: 'https://insfrontend.onrender.com',
+//      credentials: true,            //access-control-allow-credentials:true
+//      optionSuccessStatus: 200
+//   }
+app.use(cors());
 
 
 app.use(express.urlencoded());
 app.use(bodyParser.json());
 
-app.set('views', './views');
-app.set('view engine', 'ejs');
+// app.set('views', './views');
+// app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
     res.render('form.ejs');
